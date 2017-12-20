b308c91526808383e183d7514120791afcd4b875
Howard Engelhart <howard.engelhart@gmail.com>
2017-12-20 10:05:10 -0500

    updated builds to remove need for local git repo

--- build/make.sh
+++ build/make.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
-V_TAG=""
-V_ARG_ABIGEN=""
-V_ARG_BOOTNODE=""
-V_ARG_GETH=""
-
-if [[ ! -z "$1" ]]; then
-    V_TAG=$1
-    V_ARG_ABIGEN="-t abigen:${V_TAG}" 
-    V_ARG_BOOTNODE="-t bootnode:${V_TAG}"
-    V_ARG_GETH="-t geth:${V_TAG}"
-fi
-
-if [ ! -d go-ethereum ]; then
-    git clone git@github.com:ethereum/go-ethereum.git
-fi
-
-cd go-ethereum && git checkout master && git pull && git checkout $V_TAG && cd ..
-
-docker build -t abigen:latest $V_ARG_ABIGEN --force-rm -f Dockerfile.abigen . \
-    && docker build -t bootnode:latest $V_ARG_BOOTNODE -f Dockerfile.bootnode . \
-    && docker build -t geth:latest $V_ARG_GETH -f Dockerfile.geth .
+for app in geth-base geth abigen bootnode;
+do
+    v_args=""
+    if [[ ! -z "$1" ]]; then
+        v_args="-t $app:$1 --build-arg GETH_VERSION=$1"
+    fi
+    docker build -t "${app}:latest" $v_args --force-rm -f "Dockerfile.${app}" .
+    rc=$?
+    if [[ $rc != 0 ]]; then
+        exit $rc
+    fi
+done
